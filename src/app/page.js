7e902bcdb7b44b1af7da2dbd987b32fa9996d6a6
mikechess <mikecook450@gmail.com>
2023-06-05 16:34:29 -0400

    Navbar Setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar";
 
 
 export default function Home() {
   return (
-    <main>
-      Hello
+    <main className="max-w-screen-sm m-auto h-screen">
+      <Navbar />
     </main>
   )
 }
